187b981b47778cb0cc8392ec4fb8977422b84008
brian23-eng <b.odhiambo.bo@gmail.com>
2019-11-26 17:52:59 +0300

    add landing page

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -120,3 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
--- watch/views.py
+++ watch/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render,redirect
 
 def home(request):
     title = 'NeighbourHood'
-    return render(request, 'home.html', {'title': title})
+    return render(request, 'landing/home.html', {'title': title})
