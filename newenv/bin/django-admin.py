9b1322d37876952392f70216762bb3f230824fd0
brian23-eng <b.odhiambo.bo@gmail.com>
2019-11-26 15:16:12 +0300

    initialize project and app

---
+++ newenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/brian/Documents/Core/Python/Django/Neighbourhood/newenv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
