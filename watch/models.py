c6010aa401476e05401c5f7767c158625235ddf4
brian23-eng <b.odhiambo.bo@gmail.com>
2019-11-27 12:37:38 +0300

    import users class, create class models and pass in values for class neighbourhood

--- watch/models.py
+++ watch/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import datetime as dt
 
-# Create your models here.
+
+
+class Neigbourhood(models.Model):
+    name = models.CharField(max_length=120, blank=False)
+    dscription = models.TextField(max_length=500, blank=False)
+    police_number = models.IntegerField(null=True, blank=True)
+    health_number = models.IntegerField(null=True, blank=True)
+    
+    
+    
+class Profile(models.Model):
+    pass
+
+
+
+class Business(models.Model):
+    pass
+
+
+class Post(models.Model):
+    pass
