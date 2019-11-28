704136d91c8ea2dbfbd687b9a8572cca7183ada6
brian23-eng <b.odhiambo.bo@gmail.com>
2019-11-28 16:43:12 +0300

    create a method can gets all the neighbourhoods

--- watch/views.py
+++ watch/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render,redirect
+from . models import *
 
 def home(request):
     title = 'NeighbourHood'
     return render(request, 'landing/index.html', {'title': title})
+
+def hoods(request):
+    all_hoods = Neigbourhood.objects.all()
+    all_hoods = all_hoods[::-1]
+    params = {
+        'all_hoods': all_hoods
+    }
+    
+    return render(request, 'all_hoods.html', params)
