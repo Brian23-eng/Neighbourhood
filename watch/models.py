42aeec7945a174b3a93ee366f53c71ccbb52c504
brian23-eng <b.odhiambo.bo@gmail.com>
2019-11-28 11:50:11 +0300

    run test to instanstiate profile

--- watch/models.py
+++ watch/models.py
@@ -29,12 +29,20 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.username}Profile'
     
-    def save_profile(self):
-        self.save()
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+            
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
         
     def delete_profile(self):
         self.delete()
         
+    def save_profile(self):
+        self.save()
     
     
     
--- watch/tests.py
+++ watch/tests.py
@@ -4,30 +4,17 @@ from django.contrib.auth.models import User
 
 class  TestProfile(TestCase):
     def setUp(self):
-        self.user = User.objects.create( username = 'Brian',email= 'g@gmail.com', password = '123')
+        self.user = User(username = 'Brian',email= 'g@gmail.com', password = '123')
         self.user.save()
         
-        self.hood = Neigbourhood.objects.create(id =1 ,name = 'Kianda', description = 'Awesome hood', police_number = 0, health_number = 0 )
+        self.hood = Neigbourhood(name = 'Kianda', description = 'Awesome hood', police_number = 0, health_number = 0 )
         self.hood.save()
         
-        self.profile = Profile.objects.create(name = 'Brian', bio='this is a test bio',profile_picture = 'test.jpeg', location = 'kibra',user=self.user, hood = self.hood)
-        self.profile.save()
+        self.profile = Profile(user=self.user, name='Brian', bio='this is a test bio',profile_picture = 'test.jpeg', location = 'kibra', hood = self.hood)
         
     def test_instance(self):
         self.assertTrue(isinstance(self.profile, Profile))
         
-    def save_profile(self):
-        profile = Profile.objects.all()
-        self.assertTrue(len(profile)>0)
         
-        
-    def test_delete_profile(self):
-        self.profile.save_profile()
-        self.profile.delete_profile()
-        profile = Profile.objects.all()
-        self.assertTrue(len(profile) == 0)
-        
-    def tearDown(self):
-        Profile.objects.all().delete()
-        
-        
+    
+   
