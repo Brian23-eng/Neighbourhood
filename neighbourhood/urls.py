ae3f094db998be662f57a3637875a5d54b1c633a
brian23-eng <b.odhiambo.bo@gmail.com>
2019-11-29 11:26:41 +0300

    add side bar and a navbar and css links

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
+from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('watch.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/',views.logout, {"next_page": '/'}),
 ]
 
 if settings.DEBUG:
