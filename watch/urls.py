e68aa15cefd102a824ed02532f8aca819b8c36ef
brian23-eng <b.odhiambo.bo@gmail.com>
2019-11-29 10:16:21 +0300

    create a url path for joining a hood

--- watch/urls.py
+++ watch/urls.py
@@ -11,5 +11,6 @@ urlpatterns=[
     url(r'^single_hood/(?P<hood_id>\w+)', views.single_hood, name = 'single-hood'),
     url(r'^profile/(?P<username>\w+)',views.profile,name='profile'),
     url(r'^profile/(?P<username>\w+)/edit', views.edit_profile, name='edit-profile'),
+    url(r'^join_hood/(?P<id>\w+)', views.join_hood, name='join-hood')
     
 ]
