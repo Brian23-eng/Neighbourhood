6b561c3d55150b8c5effba2b1dd648c01aaf7f59
brian23-eng <b.odhiambo.bo@gmail.com>
2019-11-29 10:18:08 +0300

    create a path for leaving hood

--- watch/urls.py
+++ watch/urls.py
@@ -11,6 +11,7 @@ urlpatterns=[
     url(r'^single_hood/(?P<hood_id>\w+)', views.single_hood, name = 'single-hood'),
     url(r'^profile/(?P<username>\w+)',views.profile,name='profile'),
     url(r'^profile/(?P<username>\w+)/edit', views.edit_profile, name='edit-profile'),
-    url(r'^join_hood/(?P<id>\w+)', views.join_hood, name='join-hood')
+    url(r'^join_hood/(?P<id>\w+)', views.join_hood, name='join-hood'),
+    url(r'^leave_hood/(?P<id>\w+)', views.leave_hood, name='leave-hood')
     
 ]
