bec065342ec58bbc042563fd1e3560875bb74d5d
brian23-eng <b.odhiambo.bo@gmail.com>
2019-11-29 09:50:06 +0300

    create a url path for all_hoods

--- watch/urls.py
+++ watch/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns=[
-    url('^$', views.home, name='home')
+    url('^$', views.home, name='home'),
+    url(r'^all-hoods',views.hoods, name='hood')
 ]
