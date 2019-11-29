9c35c253bb8154ac1a0667c51ef4517c307e251e
brian23-eng <b.odhiambo.bo@gmail.com>
2019-11-29 10:11:25 +0300

    create an url path for editing profile

--- watch/urls.py
+++ watch/urls.py
@@ -10,5 +10,6 @@ urlpatterns=[
     url(r'^new-hood/', views.create_hood, name='new-hood'),
     url(r'^single_hood/(?P<hood_id>\w+)', views.single_hood, name = 'single-hood'),
     url(r'^profile/(?P<username>\w+)',views.profile,name='profile'),
+    url(r'^profile/(?P<username>\w+)/edit', views.edit_profile, name='edit-profile'),
     
 ]
